1dc26e1e2681c266288918493a7afb6f8e9cac8a
CoolShivas <shivas2710cool00@gmail.com>
2025-09-20 16:27:35 +0530

    Creating and Saving the data on MongoDB with the help of UserSCHEMA.create and by using router.post with Postman.

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,26 +1,19 @@
+import { UserSCHEMA } from "../Models/UserSCHEMA.js";
+
 export const funcRegister = async (request, response) => {
-  console.log("Printing the register function => ", request.body);
-  /**
-   * Restarting 'index.js'
-Server is running at port :-) 8000
-MongoDB Connected Successfully
-Printing the register function =>  { name: 'pari', email: 'pari@gmail.com', password: '123' }
-   */
+  const { name, email, password } = request.body;
+
+  let createUser = await UserSCHEMA.create({
+    userName: name,
+    userEmail: email,
+    userPassword: password,
+  });
+
+  console.log("Printing create user => ", createUser); // Getting data;
+
   response.json({
     message: "User register successfully...!",
     success: true,
-    data: request.body,
+    createUser,
   });
-  /**
-   * {
-    "message": "User register successfully...!",
-    "success": true,
-    "data": {
-        "name": "pari",
-        "email": "pari@gmail.com",
-        "password": "123"
-    }
-}
-   */
-  // // // Therefore, getting the desired Output on both POSTMAN and Terminal;
 };

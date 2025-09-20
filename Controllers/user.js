6f5680e02049a471c5e6ec2892d8b8dc75dc637c
CoolShivas <shivas2710cool00@gmail.com>
2025-09-20 16:45:10 +0530

    Use of if else statement to know whether the user exist already or not  with the help of UserSCHEMA.findOne.

--- Controllers/user.js
+++ Controllers/user.js
@@ -3,13 +3,28 @@ import { UserSCHEMA } from "../Models/UserSCHEMA.js";
 export const funcRegister = async (request, response) => {
   const { name, email, password } = request.body;
 
-  let createUser = await UserSCHEMA.create({
+  if (name === "" || email === "" || password === "") {
+    console.log("Please, fill all the fields.");
+    return response.json({
+      message: "Please, fill all the fields.",
+      success: false,
+    });
+  }
+
+  let createUser = await UserSCHEMA.findOne({ userEmail: email });
+
+  if (createUser) {
+    console.log("User already exists..");
+    return response.json({ message: "User already exists..", success: false });
+  }
+
+  createUser = await UserSCHEMA.create({
     userName: name,
     userEmail: email,
     userPassword: password,
   });
 
-  console.log("Printing create user => ", createUser); // Getting data;
+  //   console.log("Printing create user => ", createUser); // Getting data;
 
   response.json({
     message: "User register successfully...!",

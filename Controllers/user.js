cf4ccdc834cb92cd30d8806de73b9a3b27d21862
CoolShivas <shivas2710cool00@gmail.com>
2025-09-20 16:59:40 +0530

    Use of try and catch block for better error handling.

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,37 +1,55 @@
 import { UserSCHEMA } from "../Models/UserSCHEMA.js";
 import bcrypt from "bcryptjs";
 
+///////***********************************************************************///////
+///////***********************************************************************///////
+
+// // // Starting of User Register function;
+
 export const funcRegister = async (request, response) => {
-  const { name, email, password } = request.body;
+  try {
+    const { name, email, password } = request.body;
 
-  if (name === "" || email === "" || password === "") {
-    console.log("Please, fill all the fields.");
-    return response.json({
-      message: "Please, fill all the fields.",
-      success: false,
+    if (name === "" || email === "" || password === "") {
+      console.log("Please, fill all the fields.");
+      return response.json({
+        message: "Please, fill all the fields.",
+        success: false,
+      });
+    }
+
+    let createUser = await UserSCHEMA.findOne({ userEmail: email });
+
+    if (createUser) {
+      console.log("User already exists..");
+      return response.json({
+        message: "User already exists..",
+        success: false,
+      });
+    }
+
+    const hashPassWord = await bcrypt.hash(password, 10);
+
+    createUser = await UserSCHEMA.create({
+      userName: name,
+      userEmail: email,
+      userPassword: hashPassWord,
     });
+
+    console.log("User register successfully...!");
+
+    response.json({
+      message: "User register successfully...!",
+      success: true,
+      createUser,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.json({ message: error.message });
   }
-
-  let createUser = await UserSCHEMA.findOne({ userEmail: email });
-
-  if (createUser) {
-    console.log("User already exists..");
-    return response.json({ message: "User already exists..", success: false });
-  }
-
-  const hashPassWord = await bcrypt.hash(password, 10);
-
-  createUser = await UserSCHEMA.create({
-    userName: name,
-    userEmail: email,
-    userPassword: hashPassWord,
-  });
-
-  console.log("User register successfully...!");
-
-  response.json({
-    message: "User register successfully...!",
-    success: true,
-    createUser,
-  });
 };
+
+// // // Ending of User Register function;
+
+///////***********************************************************************///////
+///////***********************************************************************///////

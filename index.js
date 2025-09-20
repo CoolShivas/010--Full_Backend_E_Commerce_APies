c0bfecad2fd428efbbda3a6a217d6a367e405b89
CoolShivas <shivas2710cool00@gmail.com>
2025-09-20 14:59:29 +0530

    Installation and SetUp of npm i mongoose. And, connecting MongoDB to ExpressJS through Mongoose.

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const server = express();
 
 const PORT = 8000;
+
+///////***********************************************************************///////
+///////***********************************************************************///////
+
+// // // Starting of connection MongoDB to ExpressJS through Mongoose;
+
+mongoose
+  .connect(
+    "mongodb+srv://shivas2710cool00_db_user:clVb21YmcXpJHpts@cluster0.notxlqn.mongodb.net/",
+    {
+      dbName: "FullBackend_ECommerce",
+    }
+  )
+  .then(() => console.log("MongoDB Connected Successfully"))
+  .catch((err) => console.log(err));
+
+// // // Ending of connection MongoDB to ExpressJS through Mongoose;
+
+///////***********************************************************************///////
+///////***********************************************************************///////
 
 server.listen(PORT, () => {
   console.log(`Server is running at port :-) ${PORT}`);
